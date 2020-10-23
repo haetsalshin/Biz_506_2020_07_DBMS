fbadb4b03a591fbcd4d28cb1d36b7f6658aef773
haetsalshin <sinsin0902@daum.net>
2020-10-23 16:27:06 +0900

    1023

---
+++ 오라클/USER1(2020-10-23-IMAGES).sql
@@ -0,0 +1,13 @@
+-- USER1
+
+CREATE TABLE tbl_images(
+i_seq	NUMBER		PRIMARY KEY,
+i_b_seq	NUMBER	NOT NULL,	
+i_org_name	nVARCHAR2(255)	NOT NULL,
+i_file_name	nVARCHAR2(255)	NOT NULL,	
+i_down	NUMBER		
+);
+
+CREATE SEQUENCE seq_images
+START WITH 1 INCREMENT BY 1;
+
